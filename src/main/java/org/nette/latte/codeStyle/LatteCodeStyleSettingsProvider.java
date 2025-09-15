119c6476aa763a979a570cf43b40617d57bcc181
Rixafy <45132928+Rixafy@users.noreply.github.com>
2025-09-15 15:00:14 +0200

    Add LatteCodeStyleSettingsProvider.getLanguage to fix ide warning

--- src/main/java/org/nette/latte/codeStyle/LatteCodeStyleSettingsProvider.java
+++ src/main/java/org/nette/latte/codeStyle/LatteCodeStyleSettingsProvider.java
@@ -1,27 +1,33 @@
 package org.nette.latte.codeStyle;
 
 import com.intellij.application.options.*;
+import com.intellij.lang.Language;
 import com.intellij.psi.codeStyle.*;
 import org.nette.latte.LatteLanguage;
 import org.jetbrains.annotations.*;
 
 public class LatteCodeStyleSettingsProvider extends CodeStyleSettingsProvider {
     @Override
-    public CustomCodeStyleSettings createCustomSettings(CodeStyleSettings settings) {
+    public Language getLanguage() {
+        return LatteLanguage.INSTANCE;
+    }
+
+    @Override
+    public CustomCodeStyleSettings createCustomSettings(@NotNull CodeStyleSettings settings) {
         return new LatteCodeStyleSettings(settings);
     }
 
     @Nullable
     @Override
     public String getConfigurableDisplayName() {
-        return "Latte";
+        return LatteLanguage.INSTANCE.getDisplayName();
     }
 
     @NotNull
     public CodeStyleConfigurable createConfigurable(@NotNull CodeStyleSettings settings, @NotNull CodeStyleSettings modelSettings) {
         return new CodeStyleAbstractConfigurable(settings, modelSettings, this.getConfigurableDisplayName()) {
             @Override
-            protected CodeStyleAbstractPanel createPanel(CodeStyleSettings settings) {
+            protected @NotNull CodeStyleAbstractPanel createPanel(@NotNull CodeStyleSettings settings) {
                 return new LatteCodeStyleMainPanel(getCurrentSettings(), settings);
             }
         };
